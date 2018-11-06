49ac62e2e6c3cff97c60191323dd529de0f33ccc
chad <cft60@outlook.com>
2018-11-06 18:29:31 -0500

    jss

--- jss.js
+++ jss.js
@@ -1,1 +1,7 @@
-document.getElementById("MyElement").classList.toggle('darkTheme');
+const myparagraph = document.getElementById('MyElement')
+const colorbutton = document.getElementById('colorbutton')
+const usercolorinput = document.getElementById('colors')
+
+colorbutton.addEventListener('click', () => {
+  myparagraph.style.color = usercolorinput.value
+})
